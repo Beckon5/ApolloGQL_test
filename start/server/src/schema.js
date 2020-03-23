0b18e9096bb558952697a1cda142832459181b26
beckon5 <mmtaranmm@gmail.com>
2020-03-23 16:56:17 +0200

    Resolvers(STEP 3)

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,4 +1,5 @@
 const { gql } = require("apollo-server");
+const { paginateResults } = require("./utils");
 
 const typeDefs = gql`
   type Launch {
@@ -22,22 +23,54 @@ const typeDefs = gql`
 
   type Mission {
     name: String
-    missionPatch(size: PatchSize): String
+    # missionPatch(size: PatchSize): String
+    missionPatch(mission: String, size: PatchSize): String
   }
-
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
   enum PatchSize {
     SMALL
     LARGE
   }
-  type Query {
-    launches: [Launch]!
-    launch(id: ID!): Launch
-    me: User
-  }
+
   type Mutation {
     bookTrips(launchIds: [ID]!): TripUpdateResponse!
     cancelTrip(launchId: ID!): TripUpdateResponse!
     login(email: String): String # login token
+  }
+
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type Query {
+    launches( # replace the current launches query with this one.
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection { # add this below the Query type as an additional type.
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
   }
 `;
 
