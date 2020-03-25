e125763f8f68a51fe54c9beea578b34c74030650
beckon5 <mmtaranmm@gmail.com>
2020-03-25 11:10:04 +0200

    launches query fix

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -1,10 +1,32 @@
+const { paginateResults } = require("./utils");
+
 module.exports = {
   Query: {
-    launches: (_, __, { dataSources }) =>
-      dataSources.launchAPI.getAllLaunches(),
+    launches: async (_, { pageSize = 20, after }, { dataSources }) => {
+      const allLaunches = await dataSources.launchAPI.getAllLaunches();
+      // we want these in reverse chronological order
+      allLaunches.reverse();
+
+      const launches = paginateResults({
+        after,
+        pageSize,
+        results: allLaunches
+      });
+
+      return {
+        launches,
+        cursor: launches.length ? launches[launches.length - 1].cursor : null,
+        // if the cursor of the end of the paginated results is the same as the
+        // last item in _all_ results, then there are no more results after this
+        hasMore: launches.length
+          ? launches[launches.length - 1].cursor !==
+            allLaunches[allLaunches.length - 1].cursor
+          : false
+      };
+    },
     launch: (_, { id }, { dataSources }) =>
       dataSources.launchAPI.getLaunchById({ launchId: id }),
-    me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
+    me: async (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
   },
   Mission: {
     // make sure the default size is 'large' in case user doesn't specify
@@ -51,7 +73,9 @@ module.exports = {
         message: "trip cancelled",
         launches: [launch]
       };
-    }
+    },
+    uploadProfileImage: async (_, { file }, { dataSources }) =>
+      dataSources.userAPI.uploadProfileImage({ file })
   },
   Launch: {
     isBooked: async (launch, _, { dataSources }) =>
